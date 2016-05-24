b431b299aa00aa4c4d36807d1670367d4fe9f0eb
masato25 <leomasato25@gmail.com>
2016-05-24 21:14:32 +0800

    reformating

--- consulAshi/api_test.go
+++ consulAshi/api_test.go
@@ -2,37 +2,38 @@ package consulAshi
 
 import (
 	"testing"
-	. "github.com/smartystreets/goconvey/convey"
+
 	"github.com/masato25/ashi/g"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestConsulAshi(t *testing.T) {
 	g.Set("ashi_example", "../conf")
 	conf := g.Config()
 	Convey("Consul register testing", t, func() {
-    clinet := Client()
-		Convey("Gen Consul services register text", func(){
+		clinet := Client()
+		Convey("Gen Consul services register text", func() {
 			apiw := ParepareReg(conf.IP, "nignig", "owl", 4567)
 			So(apiw.Address, ShouldEqual, conf.IP)
 		})
-		Convey("Register Consul services", func(){
+		Convey("Register Consul services", func() {
 			err := ServiceRegister(conf.IP, "nignig", "owl", 4567, clinet)
 			So(err, ShouldEqual, nil)
 		})
 	})
 
-	Convey("Check Consul Server status", t, func(){
-		Convey("get return code 200", func(){
+	Convey("Check Consul Server status", t, func() {
+		Convey("get return code 200", func() {
 			scode, err := CheckConsulServer(conf.IP, 8500)
-			if err != nil{
+			if err != nil {
 				So(err, ShouldNotBeEmpty)
-			}else{
+			} else {
 				So(scode.StatusCode, ShouldEqual, 200)
 			}
 		})
-		Convey("get return code nil", func(){
+		Convey("get return code nil", func() {
 			_, err := CheckConsulServer("192.0.0.1", 8500)
-			if err != nil{
+			if err != nil {
 				So(err.Error(), ShouldContainSubstring, "Timeout")
 			}
 		})
