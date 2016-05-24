bbc437c9b57143ea35899b371d1855ba4be1161d
masato25 <leomasato25@gmail.com>
2016-05-24 21:11:49 +0800

    add logging and reformating

--- main.go
+++ main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-  "github.com/masato25/ashi/consulAshi"
-  "github.com/masato25/ashi/dockerReader"
-  "github.com/masato25/ashi/g"
-  "log"
+	"log"
+
+	"github.com/masato25/ashi/consulAshi"
+	"github.com/masato25/ashi/dockerReader"
+	"github.com/masato25/ashi/g"
 )
 
-func main(){
-  //set conf paht
-  g.Set("ashi", "./conf")
-  conf := g.Config()
-  // //get docker list
-  container := dockerReader.GetContainers(conf.DOCKERSOC)
-  client := consulAshi.Client()
-  for _, c := range container {
-    log.Println("will register", c.ID, c.Name, c.OnePort, c.IP)
-    consulAshi.ServiceRegister(conf.IP, c.Name, c.ID, c.OnePort, client)
-  }
+func main() {
+	//set conf paht
+	g.Set("ashi", "./conf")
+	conf := g.Config()
+	// //get docker list
+	container := dockerReader.GetContainers(conf.DOCKERSOC)
+	client := consulAshi.Client()
+	for _, c := range container {
+		log.Println("will register", c.ID, c.Name, c.OnePort, c.IP)
+		err := consulAshi.ServiceRegister(conf.IP, c.Name, c.ID, c.OnePort, client)
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}
 }
